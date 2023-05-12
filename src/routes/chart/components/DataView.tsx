c603a43fcb9c391ec2d3c97b0690129e45ffb8b7
alan rosales <alan.a.n.r.ar@gmail.com>
2023-05-12 14:16:47 -0300

    new changes

--- src/routes/chart/components/DataView.tsx
+++ src/routes/chart/components/DataView.tsx
@@ -10,7 +10,7 @@ const DataView = (): JSX.Element => {
 
   return (
     <div id='data-container'
-     className='w-[60%] h-[auto] flex justify-center items-center flex-col bg-gray-900 text-white'>
+     className='w-[60%] h-[65vh] flex justify-start items-center flex-col bg-[#061333] text-white'>
       <div className='w-[60%] w-full h-[50px] justify-evenly rounded-t-lg bg-[#041333] border-solid border-b-[1px] border-b-[2px] border-blue-600 flex items-center'>
         <p className={`${className}`}>Capital</p>
         <p className={`${className}`}>$</p>
@@ -19,7 +19,7 @@ const DataView = (): JSX.Element => {
         <p className={`${className}`}>Op</p>
       </div>
       <div className='w-full h-[auto] relative'>
-        <div id='files-container' className='w-full bg-[#0047e1]'>
+        <div id='files-container' className='w-full bg-[#0047e1] rounded-b-lg'>
           {chart.map((e, i) => {
             if (e.number !== 0) return (
               <DataFile
