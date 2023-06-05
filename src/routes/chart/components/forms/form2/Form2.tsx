31bb5d46f685593a9e4defbd03f5ce547a0d83c4
alan rosales <alan.a.n.r.ar@gmail.com>
2023-06-05 18:44:06 -0300

    new changes

--- src/routes/chart/components/forms/form2/Form2.tsx
+++ src/routes/chart/components/forms/form2/Form2.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import { chartStore } from '../../../../../zustand/chartStore';
 import DataPush from '../../chart-data-management/data-push';
 import DateDisplay from '../components/DateDisplay';
+import { dateStore } from '../../../../../zustand/dateStore';
 import './form2.css';
 
 export default function Form2() {
   const chart = chartStore(i => i.files);
   const addFile = chartStore(i => i.addFile);
   const [capital, setCapital] = useState<number>(0)
+  const { date } = dateStore();
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCapital(Number(event.target.value));
@@ -21,7 +23,7 @@ export default function Form2() {
       percentage: 0,
       dolar: 0,
       number: 0,
-      date: ""
+      date: date
     }
 
     addFile(item);
@@ -29,29 +31,43 @@ export default function Form2() {
   }
 
   return (
-    <form onSubmit={handleSubmit} id='initial-form' className='w-full h-full flex items-center justify-center text-white relative flex-col border-r-[1px] border-l-[1px] border-[#0047e1]'>
-      <div className='absolute flex top-[-16%] justify-evenly left-[4%] h-[30%] si items-center flex-row justify-center'>
-        <p className='montserrat'>capital date:</p>
-        <DateDisplay
-          Class={
-            'h-[80%] w-[35%] ml-[10%] montserrat second-form-date hover:bg-blue-600 text-white text-[1.04rem] border-b-[1px] border-blue-600 hover:cursor-pointer text-center hover:rounded'
-          } />
-      </div>
+    <form onSubmit={handleSubmit} id='initial-form' className='w-full h-full rounded flex items-center justify-center text-white relative flex-col border-r-[1px] border-l-[1px] border-[#0047e1]'>
+      <div id='first-form-container' className='first-form-container absolute bottom-0 w-[40%] h-[370%] bg-[#00113c] border-[1px] border-blue-600 flex flex-col justify-center items-center'>
 
-      <div className='h-[100%] w-full flex items-center justify-center justify-evenly flex-row flex-col justify-evenly'>
-        <input id='capital-form' type='number' name='numberInput' step="any" onChange={handleInputChange}
-          className='border-[1px] bg-[#061333] rounded border-[#0050ff] text-[#bfd3ff] text-center h-[30%] w-[30%] text-[1.1rem]'
-          placeholder='${ Initial Capital }' />
-        <button
-          id='chart-capital-push'
-          className='min-w-[100px] w-[20%] h-[35%] min-h-[35px] 
-          border-[1px] border-[#0047e1] rounded bg-[#002e93] hover:bg-[#0047e1]'>Submit</button>
-      </div>
-      <div className='h-[23%] min-h-[25px] w-[17%] hover:rounded hover:bg-[#0047e1]
-      flex justify-center border-b-[1px] montserrat border-[#0047e1] items-center absolute top-[-25%] right-4'
-        id='data-push'>
-        <h4 className='text-[0.7rem]'>Or Attach data</h4>
-        <DataPush />
+        <div className='h-[15%] w-full flex justify-end items-center'>
+          <div className='h-[50%] w-[30%] mr-[3%] bg-blue-600 hover:rounded hover:bg-[#0047e1]
+        flex justify-center border-b-[1px] rounded montserrat relative border-[#0047e1] items-center'
+            id='data-push'>
+            <h4 className='text-[0.7rem]'>Or Attach data</h4>
+            <DataPush />
+          </div>
+        </div>
+
+        <div className='h-[35%]  w-full flex items-center justify-center justify-evenly flex-col justify-evenly'>
+          <p className='montserrat uppercase text-[1.1rem]'>initial capital</p>
+          <input id='capital-form' type='number' name='numberInput' step="any" onChange={handleInputChange}
+            className='montserrat bg-[#0050ff57] border-[1px] border-blue-600 rounded text-[#bfd3ff] 
+            text-center h-[25%] w-[60%] text-[1.3rem]'
+            placeholder='${ input }' />
+        </div>
+
+        <div className='w-full h-[25%] flex flex-col items-center justify-center justify-evenly'>
+          <p className='montserrat uppercase'>start date</p>
+          <DateDisplay
+            Class={
+              'h-[27%] w-[20%] montserrat second-form-date hover:bg-blue-600 text-white text-[1.04rem] border-b-[1px] border-blue-600 hover:cursor-pointer text-center hover:rounded'
+            } />
+        </div>
+
+        <div className='w-full h-[25%] flex items-center justify-center'>
+          <button
+            id='chart-capital-push'
+            className='min-w-[100px] w-[60%] h-[40%] min-h-[35px] 
+            border-[1px] border-[#0047e1] rounded bg-[#002e93] hover:bg-[#0047e1]'>
+                Submit
+          </button>
+        </div>
+
       </div>
     </form>
   )
