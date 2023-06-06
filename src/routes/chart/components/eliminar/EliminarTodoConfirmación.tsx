2ab03c2fb5af8d8c007581a8a5abee44ca66ee6e
alan rosales <alan.a.n.r.ar@gmail.com>
2023-06-06 11:38:27 -0300

    new changes

--- src/routes/chart/components/eliminar/EliminarTodoConfirmación.tsx
+++ src/routes/chart/components/eliminar/EliminarTodoConfirmación.tsx
@@ -22,14 +22,14 @@ export default function EliminarTodoConfirmación() {
   }
 
   return (
-    <div className='text-white flex flex-col min-h-[160%] 
+    <div id="delete-all-files" className='montserrat text-[1.25rem] text-white flex flex-col min-h-[160%] 
     items-center justify-center absolute inset-x-0 inset-y-0 bg-[#061333] z-[5]'>
       <div className='h-[50%] w-[100%] flex items-center justify-center flex-col'>
         <p>Are you sure you want to delete ALL of the files?</p>
       </div>
       <div className='h-[50%] w-[60%] flex flex-row justify-center items-start justify-evenly'>
-        <button className='h-[40%] w-[30%] border-[2px] flex items-center justify-center border-[#de1212] hover:bg-[#a80000]' onClick={handleYes}>Yes</button>
-        <button className='h-[40%] w-[30%] border-[2px] flex items-center justify-center border-[#0047e1] hover:bg-[#0047e1]' onClick={handleNo}>No</button>
+        <button className='h-[40%] w-[30%] border-[1px] flex items-center justify-center border-[#de1212] hover:bg-[#a80000]' onClick={handleYes}>Yes</button>
+        <button className='h-[40%] w-[30%] border-[1px] flex items-center justify-center border-[#0047e1] hover:bg-[#0047e1]' onClick={handleNo}>No</button>
       </div>
     </div>
   )
