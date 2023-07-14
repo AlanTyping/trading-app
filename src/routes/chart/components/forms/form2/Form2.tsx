3c31343095249b6439e929317ca6670b3c6af010
alan rosales <alan.a.n.r.ar@gmail.com>
2023-07-14 13:44:07 -0300

    new changes

--- src/routes/chart/components/forms/form2/Form2.tsx
+++ src/routes/chart/components/forms/form2/Form2.tsx
@@ -32,7 +32,7 @@ export default function Form2() {
 
   return (
     <form onSubmit={handleSubmit} id='initial-form' className='w-full montserrat h-full rounded flex items-center justify-center text-white relative'>
-      <div id='first-form-container' className='absolute bottom-0 z-[3] w-[40%] h-[450%] bg-[#00113c] border-[1px] border-blue-600 rounded-[1em] flex flex-col justify-center items-center'>
+      <div id='first-form-container' className='absolute bottom-0 z-[3] w-[40%] h-[450%] bg-[#00113c] border-[1px] border-blue-600 rounded-[3%] flex flex-col justify-center items-center'>
 
         <div className='h-[15%] w-full flex justify-end items-center'>
           <div className='h-[50%] w-[33%] mr-[3%] bg-[#1f58d5] hover:rounded hover:bg-[#0047e1]
