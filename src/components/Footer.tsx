f359530a68b8e760170efd27fba071dd4028472e
alan rosales <alan.a.n.r.ar@gmail.com>
2023-07-19 19:35:16 -0300

    nre changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,7 +16,6 @@ export default function Footer() {
         <span className={footerState ? 'footerTextOff' : 'footerTextOn'}>Created by
           <a className={footerState ? 'footerTextOff' : 'text-[orange] border-b-[1px] border-blue-600'} href='https://www.instagram.com/alan_anr/' target='_blank'> Alan</a>
         </span>
-        <br />
         <div className={footerState ? 'footerTextOff' : 'footerTextOn'}>
           <span onClick={changeState} className={footerState ? 'footerTextOff' : 'border-b-[1px] border-blue-600 cursor-pointer footerTextOn'}>Support the development of those
             <span className={footerState ? 'footerTextOff' : 'text-[#00a0ec] footerTextOn'}> gains</span>
@@ -24,14 +23,14 @@ export default function Footer() {
         </div>
 
 
-        <div className={footerState ? 'secondContentOn' : 'secondContentOff'}>
-          <button onClick={changeState} id='footerButton' className={footerState ? 'top-[10%] left-10 absolute cursor-pointer rounded bg-[#061333] h-[40%] w-[7%]' : 'secondFooterTextOff'}>Back</button>
-          <h3 className={footerState ? 'secondFooterTextOn text-[1.5rem] text-[orange]' : 'secondFooterTextOff'}>crypto transfer</h3><br />
+        <div id='crypto-transfer' className={footerState ? 'secondContentOn' : 'secondContentOff'}>
+          <button onClick={changeState} id='footerButton' className={footerState ? 'top-[10%] left-[2%] absolute cursor-pointer rounded bg-[#061333] h-[20%] w-[7%]' : 'secondFooterTextOff'}>Back</button>
+          <h3 className={footerState ? 'secondFooterTextOn text-[1.55rem] text-[orange]' : 'secondFooterTextOff'}>crypto transfer</h3>
           <span id='cafecito' className={footerState ? 'secondFooterTextOn border-b-[1px] border-blue-600 miau' : 'secondFooterTextOff'}>TMidDcT4HvZj2U1vMzwpgdpReHcahLixTt</span>
         </div>
 
         <div className={footerState ? 'secondContentOn' : 'secondContentOff'}>
-          <h3 className={footerState ? 'secondFooterTextOn text-[1.15rem] text-[orange]' : 'secondFooterTextOff'}>Cafecito</h3>
+          <h3 className={footerState ? 'secondFooterTextOn text-[1.2rem] text-[orange]' : 'secondFooterTextOff'}>Cafecito</h3>
           <a id='cafecito' className={footerState ? 'border-b-[1px] border-blue-600 secondFooterTextOn' : 'secondFooterTextOff'} href='https://cafecito.app/alantyping' target='_blank'>Support with a coffee ☕</a>
         </div>
       </div>
