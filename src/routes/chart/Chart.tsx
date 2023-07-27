ac98da106515f3051a2f4702dfd67479a83f7435
alan rosales <alan.a.n.r.ar@gmail.com>
2023-07-27 11:46:37 -0300

    new changes

--- src/routes/chart/Chart.tsx
+++ src/routes/chart/Chart.tsx
@@ -7,11 +7,26 @@ import Footer from "../../components/Footer";
 import '../../App.css';
 
 export default function Chart() {
-  const {files} = chartStore();
+  const { files } = chartStore();
 
 
   return (
     <div className="w-full h-[auto] min-h-screen bg-[#061333] flex items-center flex-col relative">
+      {files.length > 0 ? '' : <div className="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>}
+      
       {files.length > 0 ? <LineChart /> : <div className="h-[70vh] initial-adjustment"></div>}
       {files.length > 0 ? <Dashboard /> : ''}
       <FormChange />
