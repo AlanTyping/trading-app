2b205fa11a462dbf15181f3bf32eb0a0e79e58c2
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-11-14 17:56:53 -0300

    new changes

--- src/routes/chart/components/forms/form2/Form2.tsx
+++ src/routes/chart/components/forms/form2/Form2.tsx
@@ -63,7 +63,7 @@ export default function Form2() {
 
 
         <div className='w-full flex items-center justify-center my-8 2xl:mt-[3.5vw] 2xl:mb-[2vw] mt-14'>
-          <button className='w-[50%] 2xl:text-[1.3vw] p-2 text-[1.2rem] border-[1px] border-[#0047e1] rounded bg-[#002e93] hover:bg-[#0047e1]'>
+          <button className='w-[50%] 2xl:text-[1.3vw] p-[0.35rem] text-[1.2rem] border-[1px] border-[#0047e1] rounded bg-[#002e93] hover:bg-[#0047e1]'>
             Submit
           </button>
         </div>
